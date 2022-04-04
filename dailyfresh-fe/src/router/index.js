c07ba24b702e857cf0b31ffaf16dd18dfffda2b7
Hansen <tofvoid@gmail.com>
2022-04-04 11:22:39 +0700

    router + checkout page

---
+++ dailyfresh-fe/src/router/index.js
@@ -0,0 +1,29 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import Home from '../views/Home.vue'
+import Products from '../views/Products.vue'
+import Checkout from '../views/Checkout.vue'
+
+const routes = [
+    {
+        path: '/',
+        name: 'Home',
+        component: Home,
+    },
+    {
+        path: '/Products',
+        name: 'Products',
+        component: Products,
+    },
+    {
+        path: '/Checkout',
+        name: 'Checkout',
+        component: Checkout,
+    }
+];
+
+const router = createRouter({
+    history: createWebHistory(),
+    routes: routes,
+    // linkActiveClass: 'active',
+});
+export default router
---
+++ dailyfresh-fe/src/views/Products.vue
@@ -0,0 +1,25 @@
+<template>
+  <ItemList/>
+</template>
+
+<script>
+import ItemList from '../components/ItemList.vue'
+
+export default {
+  name: 'ProductsPage',
+  components: {
+    ItemList
+  }
+}
+</script>
+
+<style>
+/* #app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+} */
+</style>
