f0aaf9dcfbe96c35876f4f0e78ef945c17e0a1c1
Hansen <tofvoid@gmail.com>
2022-05-12 17:08:33 +0700

    mulai integrasi addProduct + listProduct

--- dailyfresh-fe/src/views/ListProduct.vue
+++ dailyfresh-fe/src/views/ListProduct.vue
@@ -15,11 +15,27 @@
 
 <script>
 import ListProductSeller from "../components/ListProductSeller.vue"
+import axios from "axios"
 
 export default{
     name: 'ListPage',
+    mounted(){
+        this.fetchProducts()
+    },
     components:{
         ListProductSeller,
+    },
+    data(){
+        return{
+            products: []
+        }
+    },
+    methods: {
+        async fetchProducts(){
+            //ganti this.product jadi user_id dari login
+            const res = await axios.get(`http://localhost:8080/goods/by_seller?seller_id=${this.products}`);
+            this.goods = res.data;
+        }
     }
 }
 </script>
